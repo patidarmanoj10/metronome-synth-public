db22345c0c6b2c6bd1e36946c28059c366b2a866
Marcelo Morgado <cmarcelom@gmail.com>
2021-09-03 20:59:37 +0100

    Bootstrap mBox repo

---
+++ README.md
@@ -0,0 +1,33 @@
+# mBOX
+
+This repository contains set of smart contracts and test cases of mBOX - Metronome Syntetic Assets.
+
+## Setup
+
+1. Install
+
+   ```sh
+   git clone https://github.com/autonomoussoftware/mbox.git
+   cd mbox
+   npm install
+   ```
+
+2. set NODE_URL in env
+
+   ```sh
+   export NODE_URL=<eth mainnet url>
+   ```
+
+   or by creating a `.env` file (use `.env.template` as reference)
+
+3. Test
+
+```sh
+npm test
+```
+
+## Run test with coverage
+
+```sh
+npm run coverage
+```
---
+++ hardhat.config.ts
@@ -0,0 +1,67 @@
+import {HardhatUserConfig} from 'hardhat/types'
+import '@nomiclabs/hardhat-waffle'
+import '@nomiclabs/hardhat-ethers'
+import 'solidity-coverage'
+import 'hardhat-deploy'
+import 'hardhat-log-remover'
+import 'hardhat-gas-reporter'
+import 'hardhat-contract-sizer'
+import '@typechain/hardhat'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const config: HardhatUserConfig = {
+  defaultNetwork: 'hardhat',
+  networks: {
+    localhost: {
+      saveDeployments: true,
+    },
+    hardhat: {
+      // Note: No need to fork yet
+      // forking: {
+      //   url: process.env.NODE_URL,
+      //   blockNumber: process.env.BLOCK_NUMBER ? parseInt(process.env.BLOCK_NUMBER) : undefined,
+      // },
+      saveDeployments: true,
+    },
+    mainnet: {
+      url: process.env.NODE_URL,
+      chainId: 1,
+      gas: 6700000,
+    },
+    polygon: {
+      url: process.env.NODE_URL,
+      chainId: 137,
+      gas: 11700000,
+    },
+  },
+  paths: {
+    deployments: 'deployments',
+  },
+  namedAccounts: {
+    deployer: process.env.DEPLOYER || 0,
+  },
+  contractSizer: {
+    alphaSort: true,
+    runOnCompile: process.env.RUN_CONTRACT_SIZER === 'true',
+    disambiguatePaths: false,
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS === 'true',
+  },
+  solidity: {
+    version: '0.8.3',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  mocha: {
+    timeout: 200000,
+  },
+}
+
+export default config
