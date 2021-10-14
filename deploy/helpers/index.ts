6fe7beb9dcd913b0dcebe7017d0e31d424f82066
Marcelo Morgado <cmarcelom@gmail.com>
2021-10-14 14:48:44 +0100

    Custom admin contracts

--- deploy/helpers/index.ts
+++ deploy/helpers/index.ts
@@ -4,14 +4,15 @@ import {HardhatRuntimeEnvironment} from 'hardhat/types'
 export interface ContractConfig {
   alias: string
   contract: string
+  adminContract: string
 }
 
-export const Contracts = {
-  MBox: {alias: 'MBox', contract: 'MBox'},
-  Treasury: {alias: 'Treasury', contract: 'Treasury'},
-  DepositToken: {alias: 'DepositToken', contract: 'DepositToken'},
-  MEth: {alias: 'MEth', contract: 'SyntheticAsset'},
-  MEthDebtToken: {alias: 'MEthDebtToken', contract: 'DebtToken'},
+export const Contracts: {[key: string]: ContractConfig} = {
+  MBox: {alias: 'MBox', contract: 'MBox', adminContract: 'DefaultProxyAdmin'},
+  Treasury: {alias: 'Treasury', contract: 'Treasury', adminContract: 'DefaultProxyAdmin'},
+  DepositToken: {alias: 'DepositToken', contract: 'DepositToken', adminContract: 'DefaultProxyAdmin'},
+  MEth: {alias: 'MEth', contract: 'SyntheticAsset', adminContract: 'DefaultProxyAdmin'},
+  MEthDebtToken: {alias: 'MEthDebtToken', contract: 'DebtToken', adminContract: 'DefaultProxyAdmin'},
 }
 
 export const deterministic = async (
@@ -23,11 +24,11 @@ export const deterministic = async (
   deploy(): Promise<DeployResult>
 }> => {
   const {deployer} = await getNamedAccounts()
-  const {alias, contract} = contractConfig
+  const {alias, contract, adminContract} = contractConfig
   return await wrappedDeterministic(alias, {
     contract: contract || alias,
     from: deployer,
     log: true,
-    proxy: {proxyContract: 'OpenZeppelinTransparentProxy'},
+    proxy: {proxyContract: 'OpenZeppelinTransparentProxy', viaAdminContract: adminContract},
   })
 }
