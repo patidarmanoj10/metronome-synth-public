aaae8d4fc8cec3bb7ba650baac21e17d4b0dcec9
Marcelo Morgado <cmarcelom@gmail.com>
2022-03-10 13:45:39 +0000

    Address review request

--- contracts/oracle/ib/CTokenOracle.sol
+++ contracts/oracle/ib/CTokenOracle.sol
@@ -5,11 +5,15 @@ pragma solidity 0.8.9;
 import "../../interface/external/ICToken.sol";
 import "../../interface/oracle/IOracle.sol";
 import "../../access/Governable.sol";
+import "../../lib/OracleHelpers.sol";
+import "../../lib/WadRayMath.sol";
 
 /**
  * @title Oracle for `CTokens`
  */
 contract CTokenOracle is IOracle, Governable {
+    using WadRayMath for uint256;
+
     uint256 public constant ONE_CTOKEN = 1e8;
     /**
      * @notice The oracle that resolves the price of underlying token
@@ -28,7 +32,11 @@ contract CTokenOracle is IOracle, Governable {
     function getPriceInUsd(IERC20 _asset) external view returns (uint256 _priceInUsd) {
         address _underlyingAddress = ICToken(address(_asset)).underlying();
         uint256 _underlyinPriceInUsd = underlyingOracle.getPriceInUsd(IERC20(_underlyingAddress));
-        uint256 _underlyingAmount = (ONE_CTOKEN * ICToken(address(_asset)).exchangeRateStored()) / 1e18;
-        _priceInUsd = (_underlyinPriceInUsd * _underlyingAmount) / 1e18;
+        uint256 _underlyingAmount = OracleHelpers.normalizeUsdOutput(
+            _underlyingAddress,
+            ONE_CTOKEN * ICToken(address(_asset)).exchangeRateStored()
+        ) / 1e18;
+
+        _priceInUsd = _underlyinPriceInUsd.wadMul(_underlyingAmount);
     }
 }
