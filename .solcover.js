cf2d0864c5ec1991a01fe4fb4b1aa8e5df783b03
Marcelo Morgado <cmarcelom@gmail.com>
2022-11-30 19:47:16 +0000

    Remove unused dependencies

--- .solcover.js
+++ .solcover.js
@@ -3,7 +3,6 @@
 // - Run `hardhat clean` whenever updating `skipFiles` (Refs: https://github.com/sc-forks/solidity-coverage/issues/632#issuecomment-978037914)
 module.exports = {
   skipFiles: [
-    'dependencies/chainlink/interfaces/AggregatorV3Interface.sol',
     'dependencies/openzeppelin/access/Ownable.sol',
     'dependencies/openzeppelin/proxy/beacon/IBeacon.sol',
     'dependencies/openzeppelin/proxy/ERC1967/ERC1967Proxy.sol',
@@ -23,16 +22,10 @@ module.exports = {
     'dependencies/openzeppelin/utils/math/SafeCast.sol',
     'dependencies/openzeppelin/utils/StorageSlot.sol',
     'dependencies/openzeppelin/utils/structs/EnumerableSet.sol',
-    'dependencies/uniswap/lib/libraries/FixedPoint.sol',
-    'dependencies/uniswap/v2-core/interfaces/IUniswapV2Pair.sol',
-    'dependencies/uniswap/v2-periphery/interfaces/IUniswapV2Router01.sol',
-    'dependencies/uniswap/v2-periphery/interfaces/IUniswapV2Router02.sol',
-    'dependencies/uniswap/v2-periphery/libraries/UniswapV2OracleLibrary.sol',
-    'mock/PoolMock.sol',
-    'mock/DebtTokenMock.sol',
-    'mock/DefaultOracleMock.sol',
     'mock/ERC20Mock.sol',
     'mock/MasterOracleMock.sol',
-    'mock/RewardDistributorMock.sol',
+    'mock/PausableMock.sol',
+    'mock/PoolMock.sol',
+    'mock/TokenHolderMock.sol',
   ],
 }
