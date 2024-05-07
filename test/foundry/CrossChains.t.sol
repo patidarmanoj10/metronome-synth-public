52e809a7f8dc0824a087a9a766d36141e1a44fe6
Marcelo Morgado <cmarcelom@gmail.com>
2024-05-07 22:00:12 +0100

    Fix some tests

--- test/foundry/CrossChains.t.sol
+++ test/foundry/CrossChains.t.sol
@@ -137,8 +137,7 @@ abstract contract CrossChains_Test is Test {
     function setUp() public virtual {
         // TODO: Get from .env
         // Refs: https://github.com/autonomoussoftware/metronome-synth/issues/874
-        //mainnetFork = vm.createSelectFork("https://eth.connect.bloq.cloud/v1/peace-blood-actress");
-        mainnetFork = vm.createSelectFork("https://eth-mainnet.alchemyapi.io/v2/NbZ2px662CNSwdw3ZxdaZNe31yZbyddK");
+        mainnetFork = vm.createSelectFork("https://eth-mainnet.g.alchemy.com/v2/DAcPqBIVkeOOgYLlHxFUQ0jySiZ-k8_6");
         vm.rollFork(mainnetFork, 18262880);
 
         optimismFork = vm.createSelectFork("https://optimism-mainnet.infura.io/v3/9989c2cf77a24bddaa43103463cb8047");
